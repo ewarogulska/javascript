0bf73bab7cf8eeea48573dd5de8682f2c8bab9c1
Ewa Rogulska <ewarogulska@wp.pl>
2017-12-17 12:17:25 +0100

    data-types przykłady

--- js/5-datatypes.js
+++ js/5-datatypes.js
@@ -2,3 +2,51 @@
 
 //typy danych
 
+//mamy typy proste i złożone
+
+//proste
+//typ liczbowy
+
+var wyplata = 5000;
+var premia = 2500;
+var calkowitaWyplata;
+
+calkowitaWyplata = wyplata + premia;
+
+console.log(calkowitaWyplata);
+
+
+//typ = łańcuch znaków - string
+var wyplataStr = "5000";
+var premiaStr = "2500";
+var calkowitaWyplataStr;
+
+calkowitaWyplataStr = wyplataStr + premiaStr;
+
+console.log(calkowitaWyplataStr);
+
+
+
+//typ logiczny - wartosci true lub false
+
+var czyJestSmog;
+czyJestSmog = true;
+
+if (czyJestSmog) {
+    console.log("Jest SMOG");
+} else {
+    console.log("Nie ma SMOGu");
+}
+
+
+//typy specjalne - undefined/null  - gdy odwolujemy sie do jakiejs zmiennej ktora nie jest zdefiniowana w pamieci 
+
+var niezdefiniowanaZmienna;
+var nullowaZmienna = null;
+
+console.log(niezdefiniowanaZmienna);
+console.log(nullowaZmienna);
+
+var imieStudenta = "Michal";
+
+//...
