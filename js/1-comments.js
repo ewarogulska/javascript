6337d4d09b01f0ec001dd4af32a35dba49c101b9
Ewa Rogulska <ewarogulska@wp.pl>
2017-12-17 09:52:59 +0100

    zmienne

---
+++ js/1-comments.js
@@ -0,0 +1,5 @@
+//komentarz liniowy CTRL + lub 
+//lub Cmd +
+//oraz blokowe rozpoczynające sie od znaku
+
+//Cmd + Alt + / (Mac) CTRL + Shift + / (Windows & Linux) 
---
+++ js/2-variables.js
@@ -0,0 +1,21 @@
+'use strict';
+//przegadarka dzieki temu pilnuje rozne rzeczy - bardziej rygorystyczny sposob pisania kodu
+
+//console.log("Ewa");
+
+var imie;
+var imieDamskie = "Kaja";
+var wiek = 37;
+
+var kolor = "czerwony";
+
+imie = "Sergio";
+
+imie = imieDamskie; 
+//zostanie pobrane to co jest pod nazwa imieDamskie, czyli Kaja - czyli Sergio wyleci z pola imie a zostanie przypisana Kaja; to jest operator przypisania;
+
+console.log(imie);
+console.log(kolor);
+console.log(wiek);
+
+
