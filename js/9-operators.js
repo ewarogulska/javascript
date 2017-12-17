356b49a40a053ae66af3fcf7e1b88324cf61c86f
Ewa Rogulska <ewarogulska@wp.pl>
2017-12-17 14:55:35 +0100

    zrobiono przykłady dla operatorów logicznych

--- js/9-operators.js
+++ js/9-operators.js
@@ -1,2 +1,26 @@
 'use strict';
 
+var liczba1 = 13, liczba2 = 5;
+
+var wynik;
+
+wynik = liczba1 % liczba2;
+console.log(wynik);
+
+//to jest to samo co liczba1 = liczba1 + liczba2; co to:
+liczba1 += liczba2;
+
+console.log(liczba1);
+
+if ("2" === 2) {
+    console.log("prawda");
+} else {
+    console.log("Nieprawda");
+}
+//wyrzuci nam nieprawdę bo poza tym, że są te same cyfry to jedna jest w string - ma cudzyslow, a === to wykryje w przeciwienstwie do ==
+
+if ( !( ("2" === 2) && (8 < 4) ) ) {
+    document.write("<br> prawda");
+}
+//to sie wyswietli w oknie przegladarki-dla uzytkownika
+
