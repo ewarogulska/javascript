ef4b0759f6835d508658e6c296c3b7c0cfccf67e
Ewa Rogulska <ewarogulska@wp.pl>
2017-12-17 15:27:46 +0100

    ukończono przykłady z conditions

--- js/10-conditions.js
+++ js/10-conditions.js
@@ -1,2 +1,46 @@
 'use strict';
 
+var wzrostMateusz = 190;
+var wzrostOlgi = 180;
+
+//warunek if
+
+if ( wzrostOlgi < wzrostMateusz ) {
+    console.log("Olga jest niższa");
+}
+
+
+//warunek if else
+
+if ( wzrostOlgi > wzrostMateusz ) {
+    console.log("Olga jest wyższa");
+} else 
+    console.log("Olga jest niższa");
+
+//warunek if else if - mozmy dać dużo warunków
+if ( wzrostOlgi > wzrostMateusz ) {
+    console.log("Olga jest wyższa");
+} else if ( wzrostOlgi == wzrostMateusz ) {
+    console.log("Olga tak wysoka jak Mateusz");
+    } else {
+        console.log("Olga jest niższa");
+    }
+
+//warunek switch
+var kolor = "zielony";
+
+switch (kolor) {
+    case 'czerwony':
+        console.log('Kolor czerwony')
+        break;
+    case 'zielony':
+        console.log('Kolor zielony')
+        break;
+    case 'niebieski':
+        console.log('Kolor niebieski')
+        break;
+    default:
+        console.log('Inny kolor')
+}
+//jesli nie ma break to pojdzie dalej do kolejnego breaka
+
