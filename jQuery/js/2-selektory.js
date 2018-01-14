271853cb83767c3e38fac3218543ad463f6b3944
Ewa Rogulska <ewarogulska@wp.pl>
2018-01-14 11:17:10 +0100

    jQuery 2-selektory

---
+++ jQuery/js/2-selektory.js
@@ -0,0 +1,18 @@
+'use strict';
+
+$(function() {
+   
+    var paragrafy = $("p");
+//    console.log(paragrafy);
+//    pobierze nam wszystkie paragrafy
+
+
+//pobieramy po id 
+    var h2 = $("#second");
+//    console.log(h2);
+    
+    var paragraf = $(".paragraf");
+    console.log(paragraf);
+    
+    
+});
