602dfd2a6215018d80adc204dad555a350e4cf9b
Ewa Rogulska <ewarogulska@wp.pl>
2018-01-26 18:03:02 +0100

    kontynuacja prcy nad sliderem jQuery

--- jQuery/warsztaty/js/slider-jQuery.js
+++ jQuery/warsztaty/js/slider-jQuery.js
@@ -1,2 +1,20 @@
 'use strict';
 
+var slideShow = $("slide-show");
+//console.log(slideShow);
+
+var slideCount = $(".single-slide").length;
+console.log(slideCount);
+
+var slideWidth = $(100 / slideCount);
+console.log(slideWidth);
+
+var slideIndex = 0;
+console.log(slideIndex);
+
+$(slideShow)
+.css({"width": "500%"});
+//console.log(slideShow);
+
+$(slideShow).find(".single-slide").css("width", slideWidth * 4, "marginLeft": "slideWidth", "%");
+console.log(slideShow);
